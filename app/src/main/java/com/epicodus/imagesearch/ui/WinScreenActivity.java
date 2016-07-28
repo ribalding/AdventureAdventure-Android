d2b6d6db56cb8e7e532b25125466a699d0a4f0e5
Samuel Durfey <samuel.durfey@gmail.com>
2016-07-28 14:48:54 -0700

    Add prototype win screen.

--- app/src/main/java/com/epicodus/imagesearch/ui/WinScreenActivity.java
+++ app/src/main/java/com/epicodus/imagesearch/ui/WinScreenActivity.java
@@ -1,31 +1,54 @@
 package com.epicodus.imagesearch.ui;
 
-import android.net.Uri;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.support.v7.view.ActionMode;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.epicodus.imagesearch.R;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class WinScreenActivity extends AppCompatActivity {
-    String imageUri;
-    String imageTestUrl;
-    @Bind(R.id.winGifView) ImageView mWinGifView;
 
+    @Bind(R.id.winImageView) ImageView mWinView;
+    @Bind(R.id.winTimeView) TextView mWinTimeView;
+
+    SharedPreferences mPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win_screen);
         ButterKnife.bind(this);
 
-        imageUri = "file:///android_asset/youre_awesome.gif";
+        final String finalTime = Integer.toString(mPreferences.getInt("timeScore", 1000));
+        final String scoreDisplay = "Your final time was " + finalTime + " seconds!";
 
-        Glide.with(getApplicationContext()).load(R.drawable.youre_awesome).into(mWinGifView);
+        Picasso.with(getApplicationContext())
+                .load(R.drawable.freedom)
+                .into(mWinView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        mWinTimeView.setText(scoreDisplay);
+                    }
+
+                    @Override
+                    public void onError() {
+                        Log.d("bitch ", "DAB");
+                    }
+                });
+
+
     }
+
+
 }
